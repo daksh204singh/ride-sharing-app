9dd3116342a39293c051210587e0234c33972af1
Daksh Pratap Singh <daksh204singh@gmail.com>
2021-04-21 23:29:28 +0530

    updated

--- app/src/main/java/com/daksh/ridesharing/DriverLocationActivity.java
+++ app/src/main/java/com/daksh/ridesharing/DriverLocationActivity.java
@@ -3,7 +3,6 @@ package com.daksh.ridesharing;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
